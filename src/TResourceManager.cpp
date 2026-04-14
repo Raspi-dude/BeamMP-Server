4771ecd24308f707723c87386b62399985b6d1d1
Raspi_dude <131214288+raspi-dude@users.noreply.github.com>
2026-04-14 02:07:53 -0700

    Increase buffer size for file reading

--- src/TResourceManager.cpp
+++ src/TResourceManager.cpp
@@ -134,7 +134,7 @@ void TResourceManager::RefreshFiles() {
             size_t Read = 0;
             std::vector<char> Data;
             while (Read < FileSize) {
-                Data.resize(size_t(std::min<size_t>(FileSize - Read, 1048576)));
+                Data.resize(size_t(std::min<size_t>(FileSize - Read, 8388608)));
                 size_t RealDataSize = Data.size();
                 stream.read(Data.data(), std::streamsize(Data.size()));
                 if (stream.eof() || stream.fail()) {
